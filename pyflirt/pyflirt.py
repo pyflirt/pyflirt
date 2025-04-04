1800e9f3b406e4cd00fb00efcfd38698f22606d0
Shivanand Mishra <xemishra@gmail.com>
2025-04-04 09:18:35 +0530

    Create pyflirt.py

---
+++ pyflirt/pyflirt.py
@@ -0,0 +1,17 @@
+import random
+from .ar_flirt import ar_flirt
+from .bn_flirt import bn_flirt
+from .bg_flirt import bg_flirt
+from .hr_flirt import hr_flirt
+from .cs_flirt import cs_flirt
+from .da_flirt import da_flirt
+from .nl_flirt import nl_flirt
+from .en_flirt import en_flirt
+from .et_flirt import et_flirt
+from .tl_flirt import tl_flirt
+from .fi_flirt import fi_flirt
+from .fr_flirt import fr_flirt
+from .de_flirt import de_flirt
+from .el_flirt import el_flirt 
+from typing import get_args, Literal, Any, Generator
+from .exception import LanguageNotFoundError, TypeNotFoundError
