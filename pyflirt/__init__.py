5afbf6d1c7cfe57d8cdc17010c96b13329485e81
Shivanand Mishra <xemishra@gmail.com>
2025-04-04 16:55:22 +0530

    Update __init__.py

--- pyflirt/__init__.py
+++ pyflirt/__init__.py
@@ -2,5 +2,6 @@ from .pyflirt import loop
 from .pyjokes import get_flirt
 from .pyflirt import get_flirts
 from .pyflirt import TYPE_VALUES
+from .pyflirt import search_flirts
 from .pyflirt import LANGUAGE_VALUES
-from .pyflirt import 
+from .pyflirt import get_random_flirt
